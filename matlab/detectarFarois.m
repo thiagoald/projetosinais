4040c069ade5fa93651c4fd18336f6feba873043
unknown <tald@G3C08.windows.cin.ufpe.br>
2015-11-25 15:09:11 -0300

    adicionada funcionalidade para clicar e ressaltar a parte selecionada

--- matlab/detectarFarois.m
+++ matlab/detectarFarois.m
@@ -5,9 +5,26 @@ function [farol1 farol2] = detectarFarois( imagemCarro )
     % Aplica limiarização no canal Cr para identificar o farol
     farois = arrayfun(@checkColorRange, carroYCbCr(:,:,1), carroYCbCr(:,:,2), carroYCbCr(:,:,3));
     farois = cast(farois,'uint8');
+    % TODO: substituir por um top hat?
+    farois = imclose(farois,strel('disk',20));
+    farois = imopen(farois,strel('disk',20));
     
     % Agrupa pixels próximos
     labelFarois = bwlabel(farois);
+    
+    figure('Name', 'Selecione');
+    imshow(imagemCarro);
+    while(1)
+        coordinates = ginput(1);
+        coordinates = int64(coordinates);
+        labelSelecionada = (labelFarois == (labelFarois(coordinates(2),coordinates(1))));
+        R = imagemCarro(:,:,1).*uint8(labelSelecionada);
+        G = imagemCarro(:,:,2).*uint8(labelSelecionada);
+        B = imagemCarro(:,:,3).*uint8(labelSelecionada);
+        gray = rgb2gray(imagemCarro).*(1-uint8(labelSelecionada));
+        imagemCarroSelecionada = cat(3,gray,gray,gray)+cat(3,R,G,B);
+        imshow(imagemCarroSelecionada);
+    end
     
     % Gera imagem para ser exibida
     farolColoredLabels = label2rgb(labelFarois, 'colorcube', 'c', 'noshuffle');
